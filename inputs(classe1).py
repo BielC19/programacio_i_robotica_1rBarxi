91d59fe2aa36f1bd41b33bd3feec966e531283f1
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-09-18 09:48:06 +0200

    new file:   inputs(classe1).py 	modified:   tasca_1.py

---
+++ inputs(classe1).py
@@ -0,0 +1,6 @@
+print("Com et dius?")
+nom = input()
+print("El teu nom es:", nom)
+
+edat = input("Quants anys tens?")
+print(f"Et dius {nom}, i tens {edat} anys")
--- tasca_1.py
+++ tasca_1.py
@@ -2,3 +2,4 @@ print("Hola", "mundo")
 print( "Adiós", "mundo")
 print("el resutlat de la usma es:", 4+9)
 print("*     *", " *   *", "   *", " *   *", "*     *", sep="\n \n")
+
