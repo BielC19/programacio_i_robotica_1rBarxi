0040f805c25bced32952f9c807efa8dd78c90d6e
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-09-29 10:55:56 +0200

    modified:   tasca8.py 	new file:   tasca9.py

--- tasca8.py
+++ tasca8.py
@@ -1,11 +1,9 @@
-main.py >
+#Fes un programa que  et donada una quantitat d'ous calculi quantes capses es poden omplir amb aquests ous i et digui els sobrants (cada capsa 6 ous).
+ous = int(input("Ingrese la cantidad de huevos:"))
 
-cantidad_de_huevos = int(input("Ingrese la cantidad de huevos:
-"))
-
-cajas_llenas = cantidad_de_huevos // 6
-huevos_sobrantes = cantidad_de_huevos % 6
-if (huevos_sobrantes>0):
-  cajas_llenas += 1
-print(f"Se pueden llenar {cajas_llenas} cajas de huevos.")
-print(f"Sobran {huevos_sobrantes} huevos.")
+caixes_plenes = ous // 6
+ous_sobrants = ous % 6
+if (ous_sobrants>0):
+  caixes_plenes += 1
+print(f"Se pueden llenar {caixes_plenes} cajas de huevos.")
+print(f"Sobran {ous_sobrants} huevos.")
