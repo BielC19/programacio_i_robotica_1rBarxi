d1b44668a35beb5f017a54fa21eae62fb12616aa
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-02 09:40:08 +0200

    modified:   tasca12.py

--- tasca12.py
+++ tasca12.py
@@ -1,9 +1,9 @@
 #1- Et demani una lletra minúscula i et retorni la seva lletra majúscula. 2- Et demani una lletra majúscula i et retorni la seva lletra minúscula.
-diferencia = ord("a")-ord("A")
-minuscula = input("Introdueix el caracter en minuscula: ")
-minuscula_a_majuscula = chr(ord(minuscula)-diferencia)
-print(f"\n La teva lletra minuscula es en majuscula {minuscula_a_majuscula} i el codi ascii es {ord(minuscula)-diferencia}")
+diferencia = ord("a")-ord("A")              #mira la diferencia entre un caracter en minuscula i un en majúscula
+minuscula = input("Introdueix el caracter en minuscula: ")      #Demana el valor en mnuscula
+minuscula_a_majuscula = chr(ord(minuscula)-diferencia)          #canvia de minusucla a majúscula
+print(f"\n La teva lletra minuscula es en majuscula {minuscula_a_majuscula} i el codi ascii es {ord(minuscula)-diferencia}")    #retorna el codi
 
-majuscula = input("Introdueix un caracter en majúscula: ")
-majuscula_a_minuscula = chr(ord(majuscula)+diferencia)
-print(f"\n La teva lletra majuscula en minuscula {majuscula_a_minuscula} i el codi acsii es {ord(majuscula)+diferencia}")
+majuscula = input("Introdueix un caracter en majúscula: ")      #Demana el valor en majúscula
+majuscula_a_minuscula = chr(ord(majuscula)+diferencia)          #canvia de majúscula a minuscula
+print(f"\n La teva lletra majuscula en minuscula {majuscula_a_minuscula} i el codi acsii es {ord(majuscula)+diferencia}")       #retorna el codi
