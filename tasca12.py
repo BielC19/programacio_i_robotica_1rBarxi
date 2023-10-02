204de27abc9b86fc63439cde7721c8b03ae4d9ef
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-02 10:52:13 +0200

    modified:   tasca12.py

--- tasca12.py
+++ tasca12.py
@@ -1,6 +1,6 @@
 #1- Et demani una lletra minúscula i et retorni la seva lletra majúscula. 2- Et demani una lletra majúscula i et retorni la seva lletra minúscula.
 diferencia = ord("a")-ord("A")              #mira la diferencia entre un caracter en minuscula i un en majúscula
-minuscula = input("Introdueix el caracter en minuscula: ")      #Demana el valor en mnuscula
+minuscula = input("Introdueix el caracter en minuscula: ")      #Demana el valor en minuscula
 minuscula_a_majuscula = chr(ord(minuscula)-diferencia)          #canvia de minusucla a majúscula
 print(f"\n La teva lletra minuscula es en majuscula {minuscula_a_majuscula} i el codi ascii es {ord(minuscula)-diferencia}")    #retorna el codi
 
@@ -9,8 +9,8 @@ majuscula_a_minuscula = chr(ord(majuscula)+diferencia)          #canvia de majú
 print(f"\n La teva lletra majuscula en minuscula {majuscula_a_minuscula} i el codi acsii es {ord(majuscula)+diferencia}")       #retorna el codi
 
 
-valor = input("introdueix una lletra i te la canviara de majúscula a minuscula o al reves")
-if (valor>=65 and valor<=60):
+valor = ord(input("introdueix una lletra i te la canviara de majúscula a minuscula o al reves"))    #Demana una lletra
+if (valor>=65 and valor<=60):       #comprova si el valor es majúscula
     print(f"La convercio de majúscula a minuscula es {chr(ord(valor)+diferencia)} i el valor en codi acsii {ord(valor)+diferencia}")
-else:
+elif (valor>=97 and valor<=122):    #comprova si el valor es minuscula
     print(f"La convercio de minusucla a majúscula es {chr(ord(valor)-diferencia)} i el valor en codi acsii {ord(valor)+diferencia}")
