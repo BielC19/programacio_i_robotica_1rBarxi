b231af58a7728e25a22c39fc833629187b379683
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-02 09:34:13 +0200

    modified:   tasca12.py

--- tasca12.py
+++ tasca12.py
@@ -1,5 +1,9 @@
 #1- Et demani una lletra minúscula i et retorni la seva lletra majúscula. 2- Et demani una lletra majúscula i et retorni la seva lletra minúscula.
-minuscula = input("Introdueix la")
+diferencia = ord("a")-ord("A")
+minuscula = input("Introdueix el caracter en minuscula: ")
+minuscula_a_majuscula = chr(ord(minuscula)-diferencia)
+print(f"\n La teva lletra minuscula es en majuscula {minuscula_a_majuscula} i el codi ascii es {ord(minuscula)-diferencia}")
 
-
-#la diferencia es de 32
+majuscula = input("Introdueix un caracter en majúscula: ")
+majuscula_a_minuscula = chr(ord(majuscula)+diferencia)
+print(f"\n La teva lletra majuscula en minuscula {majuscula_a_minuscula} i el codi acsii es {ord(majuscula)+diferencia}")
