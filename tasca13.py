05e7948244146270486318ec464cf6e907c7aa88
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-03 09:18:34 +0200

    modified:   tasca13.py

--- tasca13.py
+++ tasca13.py
@@ -1,46 +1,46 @@
 
 n, m, d, e, c1, c2, s1, s2 = 1, 5, 2.5, 0.8, 'a', 'b', "hi", "ho"
 print("Comprovacio de les opercaions:")
-print("\n", n < m)                        #True
+print("\n", n < m)                  #True
 
-print("\n", d < 2.5)                #
+print("\n", d < 2.5)                #False
 
-print("\n", 'ho' == s2)             #
+print("\n", 'ho' == s2)             #True
 
-print("\n", e > 1.0)                #
+print("\n", e > 1.0)                #False
 
-print("\n", d > (e + 1.0))          #
+print("\n", d > (e + 1.0))          #True
 
-print("\n", m < int(d))             #
+print("\n", m < int(d))             #False
 
-print("\n", n == (m - 4))           #
+print("\n", n == (m - 4))           #True
 
-print("\n", len(c1) > 90)           #
+print("\n", ord(c1) > 90)           #True
 
-print("\n", s1 == "sl")             #
+print("\n", s1 == "sl")             #False
 
-print("\n", c1 >= 'a')              #
+print("\n", c1 >= 'a')              #True
 
-print("\n", m >= 5)                 #
+print("\n", m >= 5)                 #True
 
-print("\n", int (d + e) > 3)        #
+print("\n", int (d + e) > 3)        #False
 
-print("\n", c2 == 'c')              #
+print("\n", c2 == 'c')              #False
 
-print("\n", s1 >= s2)               #
+print("\n", s1 >= s2)               #False
 
-print("\n", len(c2) != 98)          #
+print("\n", ord(c2) != 98)          #False
 
-print("\n", s2 >= "hola")           #
+print("\n", s2 >= "hola")           #False
 
-print("\n", s1 == "hi there!")      #
+print("\n", s1 == "hi there!")      #False
 
-print("\n", e != d)                 #
+print("\n", e != d)                 #True
 
-print("\n", s1 != s1)               #
+print("\n", s1 != s1)               #False
 
-print("\n", n + 2 < m - 3)          #
+print("\n", n + 2 < m - 3)          #False
 
-print("\n", 3 < int (d + e))        #
+print("\n", 3 < int(d + e))         #False
 
-print("\n", "c1" == s2)             #
+print("\n", "c1" == s2)             #False
