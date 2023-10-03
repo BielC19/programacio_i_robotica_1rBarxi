9c6b67a516e8557519c961252c3cc5f20eeafe4e
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-03 09:07:13 +0200

    modified:   tasca13.py

--- tasca13.py
+++ tasca13.py
@@ -1,24 +1,46 @@
 
 n, m, d, e, c1, c2, s1, s2 = 1, 5, 2.5, 0.8, 'a', 'b', "hi", "ho"
-print("\n", n < m)
-print("\n", d < 2.5)
-print("\n", 'ho' == s2)
-print("\n", e > 1.0)
-print("\n", d > (e + 1.0))
-print("\n", m < int(d))
-print("\n", n == (m - 4))
-print("\n", len(c1) > 90)
-print("\n", s1 == "sl")
-print("\n", c1 >= 'a')
-print("\n", m >= 5)
-print("\n", int (d + e) > 3)
-print("\n", c2 == 'c')
-print("\n", s1 >= s2)
-print("\n", len(c2) != 98)
-print("\n", s2 >= "hola")
-print("\n", s1 == "hi there!")
-print("\n", e != d)
-print("\n", s1 != s1)
-print("\n", n + 2 < m - 3)
-print("\n", 3 < int (d + e))
-print("\n", "c1" == s2)
+print("Comprovacio de les opercaions:")
+print("\n", n < m)                        #True
+
+print("\n", d < 2.5)                #
+
+print("\n", 'ho' == s2)             #
+
+print("\n", e > 1.0)                #
+
+print("\n", d > (e + 1.0))          #
+
+print("\n", m < int(d))             #
+
+print("\n", n == (m - 4))           #
+
+print("\n", len(c1) > 90)           #
+
+print("\n", s1 == "sl")             #
+
+print("\n", c1 >= 'a')              #
+
+print("\n", m >= 5)                 #
+
+print("\n", int (d + e) > 3)        #
+
+print("\n", c2 == 'c')              #
+
+print("\n", s1 >= s2)               #
+
+print("\n", len(c2) != 98)          #
+
+print("\n", s2 >= "hola")           #
+
+print("\n", s1 == "hi there!")      #
+
+print("\n", e != d)                 #
+
+print("\n", s1 != s1)               #
+
+print("\n", n + 2 < m - 3)          #
+
+print("\n", 3 < int (d + e))        #
+
+print("\n", "c1" == s2)             #
