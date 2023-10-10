e84c9b6b1d4cebae60b51339d6cbfd0029ace461
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-10 09:24:45 +0200

    modified:   examen1.0ex2.py

---
+++ examen1.0ex2.py
@@ -0,0 +1,14 @@
+#2- Fer amb CodeBlocks (5 punts):
+#1 - Fer un programa que donada una dada en aquest format: ABAB et calculi el caràcter
+#l’equivalent a la taula d’ASCII del nùmero AA i el BB. Exemple de la posició de les dades.
+#A B A B
+#Exemple si l'AA fos un 24 i el BB fos un 15 estarien així.
+#2 1 4 5
+#2- Demana a l’usuari l’inicial del nom i del primer cognom (en majúscules) i codifica’l
+#utilitzant la taula ASCII amb el següent mode d’encriptació:
+#● Valor numèric ASCII del caracter - 5
+#4- Mostrar a l’usuari els caràcters encriptats amb els caràcters d’ASCII nous.
+
+numeros = input("Introdueix 4 caracters junts: ")
+
+primer = numeros
