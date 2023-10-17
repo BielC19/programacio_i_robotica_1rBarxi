a6ddbbb41048c8cd8d8a917e0e20a22981f25091
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-17 09:27:36 +0200

    modified:   tasca16.py

--- tasca16.py
+++ tasca16.py
@@ -1,10 +1,10 @@
 #Avaluar les següents expressions amb diferents valors de a,b i c (a i b tipus int, c tipus string) i explica els problemes que trobes.
 
-a = int(input())
-b = int(input())
-c = input()
+a = int(input("introdueix el primer numero:"))
+b = int(input("introdueix el segon numero:"))
+c = input("introdueix una lletra:")
 
-c != 'a' or c != 'b'
-a < 1 or a > -1
-c != 'a' or c == 'a'
-b < a or b > a
+print(c != 'a' or c != 'b')         #Si la lletre que li dones a la variable c no es 'a' o no es 'b' et dona True es compleix sempre
+print(a < 1 or a > -1)              #Si el valor de a es mes petit que 1 o es mes gran que -1 es compoleix sempre
+print(c != 'a' or c == 'a')         #Si la lletra que demanes a la variable c es diferent a 'a' o iugal que 'a' es compleix sempre
+print(b < a or b > a)               #Si el valor de b es mes petit que el que a o b es mes gran que a aixi que es compleix sempre que no siguin iguals els dos valors
