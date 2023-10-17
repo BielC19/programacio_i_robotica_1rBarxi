882b28a647a1c118c1063aeb6ccd48d5a293088c
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-17 09:06:59 +0200

    new file:   tasca16.py 	new file:   tasca17.py

---
+++ tasca16.py
@@ -0,0 +1,10 @@
+#Avaluar les següents expressions amb diferents valors de a,b i c (a i b tipus int, c tipus string) i explica els problemes que trobes.
+
+a = int(input())
+b = int(input())
+c = input()
+
+c != 'a' or c != 'b'
+a < 1 or a > -1
+c != 'a' or c == 'a'
+b < a or b > a
