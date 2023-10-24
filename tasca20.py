765fc3217522732e41b3e4bef94e1021ce2b5045
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-24 09:13:54 +0200

    modified:   tasca20.py

--- tasca20.py
+++ tasca20.py
@@ -1,18 +1,17 @@
 #Fes un programa que donada una entrada en format HH:MM:SS
 #tu mAadre tiene una polla que ya la quisierta yo me dio pena por tu opadreEt retorni el mateix número a la sortida sumant 1 segon.
 HH = int(input("indtroueix la hora en format HH: "))
-MM = int(input(""))
-SS = int(input(""))
+MM = int(input("Introdueix els minuts en format MM: "))
+SS = int(input("Introdueix els segons en format SS: "))
 
-if SS>=60:
-    MM += 1
-if MM>=60:
-    HH += 1
+
 if HH==24 and MM==60 and SS==60:
     HH = 00
     MM = 00
     SS = 00
-else:
-    HH = HH-24
-    
+elif HH>=24 and MM>=60 and SS>=60:
+    HH = HH + (HH-24)
+    MM = 00 + (MM-60)
+    SS = 00 + (SS-60)
+
 print(f"La hora es {HH}:{MM}:{SS}")
