af14f939ba598c9deb78e10d213f6ad3f965b61b
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-10-27 11:43:14 +0200

    modified:   tasca22.py

--- tasca22.py
+++ tasca22.py
@@ -2,44 +2,36 @@
 #El programa ha de demanar a l'usuari que ingressi dos números i després li preguntarà quina operació desitja realitzar.
 #No pot dividir entre 0. Depenent de l'operació seleccionada, el programa farà el càlcul i mostrarà el resultat.
 #Importo la biblioteca para crear el menu interactivo
-import os
 
-while True:
-    print()
-    print("Calculator")
-    print("1 - Suma")
-    print("2 - Resta")
-    print("3 - Multiplicación")
-    print("4 - División")
-    print("5 - Salir del programa")
-    opcion = input("\n\nSelect an option (number): ")
-    if int(opcion) == 1:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        result_suma = num1 + num2
-        print("%.2f" %result_suma)
-    elif int(opcion) == 2:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        result_resta = num1 - num2
-        print("%.2f" %result_resta)
-    elif int(opcion) == 3:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        result_multiplicacion = num1 * num2
-        print("%.2f" %result_multiplicacion)
-    elif int(opcion) == 4:
-        num1 = float(input("Enter the dividend: "))
-        num2 = float(input("Enter the divisor: "))
+print("\n")
+print("Calculator")
+print("1 - Suma")
+print("2 - Resta")
+print("3 - Multiplicación")
+print("4 - División")
+print("5 - Salir del programa")
+opcion = input("\n\nSelect an option (number): ")
+if int(opcion) == 1:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result_suma = num1 + num2
+    print("%.2f" %result_suma)
+elif int(opcion) == 2:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result_resta = num1 - num2
+    print("%.2f" %result_resta)
+elif int(opcion) == 3:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result_multiplicacion = num1 * num2
+    print("%.2f" %result_multiplicacion)
+elif int(opcion) == 4:
+    num1 = float(input("Enter the dividend: "))
+    num2 = float(input("Enter the divisor: "))
     try:
         result_division = num1 / num2
         print("%.2f" %result_division)
     except ZeroDivisionError:
         print ("No se puede dividir entre cero.")
-    if __name__=="__main__":
-        os._exit(0)
-    if __name__=='__main__':
-        import sys
-        sys.exit(0)
-    if __name__=="__main__":
-        exit()
+
