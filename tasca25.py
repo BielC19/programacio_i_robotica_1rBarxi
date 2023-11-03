27d8fb9ba95359f01a40a5b843b2962d5768c84e
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-11-03 11:06:51 +0100

    modified:   tasca25.py

--- tasca25.py
+++ tasca25.py
@@ -5,20 +5,24 @@
 #En los divisibles por 3 y 5, hay que escribir "FizzBuzz".
 i = 0
 while i<=100:
-    if (i % 3 == 0) and (i % 5 == 0):
-        print("FizzBuzz", end=" ")
+    if i==100:
+        print("Buzz", end="")
+    elif (i % 3 == 0) and (i % 5 == 0):
+        print("FizzBuzz,", end=" ")
     elif i % 3 == 0:
-        print("Fizz", end=" ")
+        print("Fizz,", end=" ")
     elif i % 5 == 0:
-        print("Buzz", end = " ")
+        print("Buzz,", end = " ")
     else:
-        print(i, end =" ")
+        print(i, end =", ")
     i += 1
 print("\n\n")
 i = 0
 a = []
 while i<=100:
-    if (i % 3 == 0) and (i % 5 == 0):
+    if i==100:
+        print("Buzz", end="")
+    elif (i % 3 == 0) and (i % 5 == 0):
         a.append("FizzBuzz")
     elif i % 3 == 0:
         a.append("Fizz")
